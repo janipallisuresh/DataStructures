fe9cf80e1fbec80a5897b46e907a8602d2f575e8
janipallisuresh <janipallisuresh@gmail.com>
2016-01-26 20:43:47 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,13 +1,22 @@
+/* Testing Single Linked List Code */
 #include "singlelinkedlist.h"
 
 int main()
 {
 	int i=10;
 	node* head = NULL;
-		remove_node(4, &head);
-		reverse_list(&head);
-		head = remove_node_recursive(1, &head);
-		reverse_list_recursive(&head);
+	node* temp = NULL;
+	node* temp1 = NULL;
+	int count = 1;
+	
+	remove_node(4, &head);
+	head = remove_node_recursive(1, &head);
+	display_list(head);
+	reverse_list(&head);
+	reverse_list_recursive(&head);
+	reverse_list_group(3, &head);
+	find_remove_loop(&head);
+
 	for(i=10; i>0; i--) {
 		add_node(i, &head);
 	}
@@ -20,7 +29,22 @@ int main()
 	head = remove_node_recursive(10, &head);
 	reverse_list(&head);
 	display_list(head);
-	remove_node(2, &head);
+	reverse_list_group(3, &head);
 	display_list(head);
+	
+	/* make a loop to test the function */
+	temp = head;
+	while(temp->link != NULL) 
+	{
+	    if(count == 4)
+	     temp1 = temp;
+	    temp = temp->link;
+	    count++;
+	}
+	temp->link = temp1;
+
+	printf("Loop at Node: %d\n", find_remove_loop(&head));
+	display_list(head);
+	printf("Loop at Node: %d\n", find_remove_loop(&head));
 	return 0;
 }
